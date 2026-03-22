d7c85fd55e908d5b31ad2021330d76a11e2b8bca
arm-architecture <halorich96@live.com.mx>
2026-03-21 19:11:56 -0600

    se realizan implementaciones de vistas principales y se agrega soporte para peticiones

--- CRM/CRM/urls.py
+++ CRM/CRM/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #esta instruccion indica que todas las urls que estan
+    #especificadas en el archivo urls_dashboard.py 
+    path('', include('dashboard.urls_dashboard')),
 ]
--- CRM/dashboard/views.py
+++ CRM/dashboard/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
 
-# Create your views here.
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def login(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        return redirect('dashboard')
+    return render(request, 'login.html')
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
+
+def account_details(request):
+    return render(request, 'account_details.html')
