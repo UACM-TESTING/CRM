ac941f9c20f6c3b3b38a7742ca6c0897574bdc59
arm-architecture <halorich96@live.com.mx>
2026-05-01 21:57:11 -0600

    se cambia de sqlite3 a postgresql, se agregan migraciones y se actualizan los archivos .pyc para la nueva versión de python.

--- CRM/dashboard/urls_dashboard.py
+++ CRM/dashboard/urls_dashboard.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('account_details/', views.account_details, name='account_details'),
+    path('consulta/', views.consulta, name='consulta'),
 ]
--- CRM/dashboard/views.py
+++ CRM/dashboard/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
+from .db_connection import DatabaseConnection
+from django.shortcuts import render, redirect
+from .models import Producto
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'html/dashboard.html')
 
 def login(request):
     
@@ -10,11 +12,24 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         return redirect('dashboard')
-    return render(request, 'login.html')
+    return render(request, 'html/login.html')
 
 def logout(request):
     auth_logout(request)
     return redirect('login')
 
 def account_details(request):
-    return render(request, 'account_details.html')
+    return render(request, 'html/account_details.html')
+
+def consulta(request):
+    
+    database = DatabaseConnection()
+    database.connect()
+    # print("Conexión establecida" + str(database.connection))
+    
+    lista = database.fetchall("SELECT * FROM productos;")
+    productos = []
+    for x in range(len(lista)):
+        productos.append(lista[x])
+        print(productos)
+    return render(request, 'html/consulta.html', context={'productos': productos})
